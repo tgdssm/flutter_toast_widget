10df74be016ec12a54173441af2f1202f50de209
tgdssm <thalissongabriel1999@gmail.com>
2023-04-05 20:11:28 -0300

    test

--- test/toast_test.dart
+++ test/toast_test.dart
@@ -1,6 +1,43 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:toast/toast.dart';
+import 'package:flutter_toast_widget/src/toast_widget.dart';
 
 void main() {
+  Future click(WidgetTester tester) async {
+    await tester.tap(find.byKey(const Key("test_key")));
+    await tester.pump();
+    await tester.pump();
+    await tester.pump();
+  }
+
+  testWidgets('flutter toast widget test', (widgetTester) async {
+    await widgetTester.pumpWidget(
+      MaterialApp(
+        home: Material(
+          child: Builder(
+            builder: (context) {
+              return Scaffold(
+                body: Container(
+                  height: 500,
+                  width: 500,
+                  alignment: Alignment.center,
+                  child: MaterialButton(
+                    key: const Key("test_key"),
+                    onPressed: () {
+                      ToastWidget.show(context: context, message: "Test");
+                    },
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+    await click(widgetTester);
+    expect(find.byType(Text), findsOneWidget);
+    await widgetTester.pump(const Duration(seconds: 2));
+    await widgetTester.pump();
+    expect(find.byType(Text), findsNWidgets(0));
+  });
 }
